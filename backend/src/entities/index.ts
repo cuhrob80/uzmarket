42005d792c9045633f21df0e19d3021542f0907d
cuhrob80 <cuhrob80@gmail.com>
2026-09-10 00:32:46 +0300

    Export listing favorite entity

--- backend/src/entities/index.ts
+++ backend/src/entities/index.ts
@@ -2,3 +2,5 @@ export { Category } from './category.entity';
 export { Listing, ListingJobType, ListingStatus } from './listing.entity';
 export { ListingImage } from './listing-image.entity';
 export { User } from './user.entity';
+
+export { ListingFavorite } from './listing-favorite.entity';
